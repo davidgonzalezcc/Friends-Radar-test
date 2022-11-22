07e4ae87af8507bccfa26ee6946e4ef14a193f1b
Laura <79776871+Lau2906@users.noreply.github.com>
2022-11-22 16:44:47 -0500

    add mapa

--- app/src/main/java/com/example/friendsradar/pantallaMapa.java
+++ app/src/main/java/com/example/friendsradar/pantallaMapa.java
@@ -1,14 +1,72 @@
 package com.example.friendsradar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class pantallaMapa extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class pantallaMapa extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LatLng start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_mapa);
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+
+
+
+
+        // Add a marker in Sydney and move the camera
+        start = new LatLng(  4.627835831777713, -74.06409737200865);
+        mMap.addMarker(new MarkerOptions().position(start).title("Start").alpha(0.75f)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(start));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 14));
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(@NonNull LatLng latLng) {
+
+                double dist = distance(start.latitude, start.longitude, latLng.latitude, latLng.longitude);
+
+                Toast.makeText(pantallaMapa.this, "Distance is: " + dist + " km", Toast.LENGTH_LONG).show();
+
+                mMap.addMarker(new MarkerOptions().position(latLng).title("End").alpha(0.8f).snippet(dist+" km").
+                        icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+
+
+            }
+        });
+    }
+
+    private double distance(double myLat, double myLong, double otherLat, double otherLong){
+        double latDistance = Math.toRadians(myLat - otherLat);
+        double longDistance = Math.toRadians(myLong - otherLong);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(myLat)) *
+                Math.cos(Math.toRadians(otherLat)) * Math.sin(longDistance / 2) * Math.sin(longDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double res = 6371.01 * c;
+
+        return Math.round(res * 100.0) / 100.0;
+    }
 }
+
